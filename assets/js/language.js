23c452cfe60725382fd4497f80f435a17cea3200
seleop <seleop@naver.com>
2024-12-04 11:07:37 +0900

    First Commit

---
+++ assets/js/language.js
@@ -0,0 +1,180 @@
+const korean = {
+    category : "카테고리",
+    DISCOVER1 : "신규 입고 상품",
+    DISCOVER2 : "베스트 50위 상품",
+    DISCOVER3 : "블랙 프라이데이 최대 80% 할인",
+    DISCOVER4 : "캐시미어",
+    DISCOVER5 : "특별 할인",
+    DISCOVER6 : "1+1",
+    MORESIZE1 : "작은 사이즈",
+    MORESIZE2 : "큰 사이즈",
+    WOMANCLOTHING1 : "드레스 & 점프수트",
+    WOMANCLOTHING2 : "스웨터",
+    WOMANCLOTHING3 : "상의",
+    WOMANCLOTHING4 : "블라우스",
+    WOMANCLOTHING5 : "바지",
+    WOMANCLOTHING6 : "진",
+    WOMANCLOTHING7 : "코트 & 자켓",
+    WOMANCLOTHING8 : "블레이저",
+    WOMANCLOTHING9 : "수트",
+    WOMANCLOTHING10 : "반바지 & 치마",
+    WOMANCLOTHING11 : "티셔츠",
+    WOMANCLOTHING12 : "셔츠",
+    WOMANCLOTHING13 : "투피스 드레스",
+    WOMANCLOTHING14 : "조끼",
+    WOMANCLOTHING15 : "전체보기",
+    SHOESACCESSORIES1 : "주얼리",
+    SHOESACCESSORIES2 : "겨울 악세서리",
+    SHOESACCESSORIES3 : "신발",
+    SHOESACCESSORIES4 : "가방",
+    SHOESACCESSORIES5 : "벨트",
+    SHOESACCESSORIES6 : "스카프",
+    SHOESACCESSORIES7 : "장갑",
+    SHOESACCESSORIES8 : "모자",
+    SHOESACCESSORIES9 : "양말",
+    SHOESACCESSORIES10 : "선글라스",
+    SHOESACCESSORIES11 : "장신구 전체보기",
+    COMMUNITY1 : "이벤트",
+    COMMUNITY2 : "공지사항",
+    COMMUNITY3 : "리뷰",
+    COMMUNITY4 : "SNS",
+    COMMUNITY5 : "고객센터",
+    navMenu00 : "신규 입고 상품",
+    navMenu01 : "베스트 50위 상품",
+    navMenu02 : "블랙 프라이데이 최대 80% 할인",
+    navMenu03 : "캐시미어",
+    navMenu04 : "특별 할인",
+    navMenu05 : "1 + 1",
+    couponbanner1 : "로그인하고",
+    couponbanner2 : "쿠폰 받아가세요!",
+    followbanner1 : "SNS에 팔로우하고",
+    followbanner2 : "10% 할인 받아가세요!",
+    newitemtitle1 : "신규 상품 구매시",
+    newitemtitle2 : "7% 추가 할인!",
+    hotitemtitle1 : "베스트 50위 상품",
+    hotitemtitle2 : "최대 50% 할인!",
+    cashmere1 : "캐시미어",
+    cashmere2 : "100% 프리미엄",
+    cashmere3 : "원단 사용!",
+    cashmere4 : "캐시미어 마니아들을 위한 데일리 룩.",
+    outromsg1 : "항상 최선을 다하자.... 멋진 퍼블리셔가 되기 위해!",
+    outromsg2 : "J.HEEDO는 제가 현재 개발 중인 사이트 이름으로, 고객 만족과 사용 편의성을 최우선으로 하는 1인 쇼핑몰 브랜드입니다. 고객들이 자신 있게 착용할 수 있는 깔끔하고 세련된 의류를 제작, 포장, 배송하기 위해 항상 최선을 다하고 있습니다.",
+    outromsg3 : "우리의 목표는 고객이 신뢰하고 다시 찾는 브랜드를 만드는 것입니다. 원단 선택부터 포장의 마지막 손길까지 모든 디테일에 세심하게 신경 쓰며, 최고의 품질을 보장하기 위해 노력하고 있습니다.",
+};
+
+const english = {
+    category : "CATEGORY",
+    DISCOVER1 : "NEW 7%",
+    DISCOVER2 : "BEST 50",
+    DISCOVER3 : "BLACK FRIDAY ~80%",
+    DISCOVER4 : "CASHMERE",
+    DISCOVER5 : "SPECIALS",
+    DISCOVER6 : "1+1",
+    MORESIZE1 : "Petites",
+    MORESIZE2 : "Tall",
+    WOMANCLOTHING1 : "Dresses & Jumpsuits",
+    WOMANCLOTHING2 : "Sweaters",
+    WOMANCLOTHING3 : "Tops",
+    WOMANCLOTHING4 : "Blouses",
+    WOMANCLOTHING5 : "Pants",
+    WOMANCLOTHING6 : "Jeans",
+    WOMANCLOTHING7 : "Coats & Jackets",
+    WOMANCLOTHING8 : "Blazers",
+    WOMANCLOTHING9 : "Suits",
+    WOMANCLOTHING10 : "Skirts & Shorts",
+    WOMANCLOTHING11 : "T-shirts",
+    WOMANCLOTHING12 : "Shirting",
+    WOMANCLOTHING13 : "Two-Piece Dressing",
+    WOMANCLOTHING14 : "Vests",
+    WOMANCLOTHING15 : "View All",
+    SHOESACCESSORIES1 : "Jewely",
+    SHOESACCESSORIES2 : "Winter Accessories",
+    SHOESACCESSORIES3 : "Shoes",
+    SHOESACCESSORIES4 : "Bags",
+    SHOESACCESSORIES5 : "Belts",
+    SHOESACCESSORIES6 : "Scarves",
+    SHOESACCESSORIES7 : "Gloves",
+    SHOESACCESSORIES8 : "Hats",
+    SHOESACCESSORIES9 : "Socks",
+    SHOESACCESSORIES10 : "Sunglasses",
+    SHOESACCESSORIES11 : "View All Accessories",
+    COMMUNITY1 : "Events",
+    COMMUNITY2 : "Notice",
+    COMMUNITY3 : "Reviwes",
+    COMMUNITY4 : "SNS",
+    COMMUNITY5 : "Help",
+    navMenu00 : "NEW 7%",
+    navMenu01 : "BEST 50",
+    navMenu02 : "BLACK FRIDAY ~80%",
+    navMenu03 : "CASHMERE",
+    navMenu04 : "SPECIALS",
+    navMenu05 : "1 + 1",
+    couponbanner1 : "Sign in &",
+    couponbanner2 : "Get a Coupon",
+    followbanner1 : "Follow & Like",
+    followbanner2 : "Get 10% OFF",
+    newitemtitle1 : "NEW ITEM",
+    newitemtitle2 : "7% PRICE OFF",
+    hotitemtitle1 : "BEST 50 ITEMS",
+    hotitemtitle2 : "~ 50% Price OFF",
+    cashmere1 : "Cashmere",
+    cashmere2 : "100% PREMIUM",
+    cashmere3 : "WOOL",
+    cashmere4 : "Daily look for Mania",
+    outromsg1 : "DO MY BEST ALWAYS.... BECOME TO COOL PUBLISHER",
+    outromsg2 : "J.HEEDO, the name of the site I am currently developing, is a one-person shopping mall brand that prioritizes customer satisfaction and ease of use above all else. We are committed to always doing our best to produce, package, and deliver clean and sophisticated clothing that our customers can wear with confidence.",
+    outromsg3 : "Our goal is to create a brand that customers trust and return to time and time again. We pay meticulous attention to every detail, from fabric selection to the final touch of packaging, ensuring the highest quality in every product.",
+};
+
+const languagebtn = document.querySelectorAll('.languagewrap span');
+
+// let isKorean = false;
+
+const changeLanguage = (language) => {
+    const texts = document.querySelectorAll('[data-key]');
+    texts.forEach((ele) => {
+        let data = ele.getAttribute(["data-key"]);
+        ele.innerHTML = language[data];
+    })
+}
+
+
+
+for(let i=0; i<languagebtn.length;i++){
+    languagebtn[i].addEventListener('click', () => {
+        const bannerimgs = document.querySelectorAll('.bannerimg');
+        languagebtn.forEach(ele =>{
+            ele.classList.remove('active')
+        })
+        languagebtn[i].classList.add('active');
+        if(languagebtn[i].innerHTML === "KR") {
+            /* 한글로 변경하는 함수 */
+            changeLanguage(korean);
+            document.querySelectorAll("[data-key]").forEach((ele) => {
+                ele.classList.add('active');
+            })
+            document.querySelector('.spanwrap1').classList.add('active');
+            for(let j = 0 ; j <bannerimgs.length ; j++){
+                bannerimgs.forEach((el, j) => {
+                    el.src = `../img/banner/banner${j}kor.png`
+                    console.log(j);
+                })
+            }
+        }
+        else if (languagebtn[i].innerHTML === "EN"){
+            /* 영어로 변경하는 함수 */
+            changeLanguage(english);
+            document.querySelectorAll("[data-key]").forEach((ele) => {
+                ele.classList.remove('active');
+            })
+            document.querySelector('.spanwrap1').classList.remove('active');
+            for(let k = 0 ; k <bannerimgs.length ; k++){
+                bannerimgs.forEach((el, k) => {
+                    el.src = `../img/banner/banner${k}eng.png`
+                })
+            }
+        } else {
+            /* 그 외 추가되는 언어로 변경하는 함수 (예정) */
+        }
+    })
+}
---
+++ assets/js/main.js
@@ -0,0 +1,310 @@
+gsap.registerPlugin(ScrollTrigger);
+/* DOM */
+const categoryImgsrc = ["../img/preview/cashmere.jpg",
+    "../img/preview/petites.jpg",
+    "../img/preview/tall.jpg",
+    "../img/preview/jumpsuit.jpg", 
+    "../img/preview/sweater.jpg", 
+    "../img/preview/tops.jpg", 
+    "../img/preview/blouse.jpg",
+    "../img/preview/pants.jpg",
+    "../img/preview/jeans.jpg",
+    "../img/preview/coats.jpg",
+    "../img/preview/blazer.jpg",
+    "../img/preview/suit.jpg",
+    "../img/preview/skirt.jpg",
+    "../img/preview/t-shirts.jpg",
+    "../img/preview/shirting.jpg",
+    "../img/preview/two-piece-dress.jpg",
+    "../img/preview/vest.jpg",
+    "../img/preview/jewely.jpg",
+    "../img/preview/winter-accessory.jpg",
+    "../img/preview/shoes.jpg",
+    "../img/preview/bag.jpg",
+    "../img/preview/belt.jpg",
+    "../img/preview/scarves.jpg",
+    "../img/preview/gloves.jpg",
+    "../img/preview/hat.jpg",
+    "../img/preview/socks.jpg",
+    "../img/preview/sunglasses.jpg",];
+
+const remoteicons = document.querySelectorAll('.remotemainicon');
+const toSections = document.querySelectorAll('.controller');
+
+
+
+const modifyImgFilter = () => {
+    document.querySelector(".imagebox_for_relative").addEventListener("mouseover", () => {
+        document.querySelector("#stickyimg").classList.add("active");
+    });
+    document.querySelector(".imagebox_for_relative").addEventListener("mouseleave", () => {
+        document.querySelector("#stickyimg").classList.remove("active");
+    });
+};
+
+const animateSection3 = () => {
+    let section3tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".section3",
+            start: "center center",
+            end: "+=1400",
+            scrub: true,
+            pin: true,
+        },
+    });
+    section3tl
+        .fromTo(".videowrap", { height: "480px" }, { height: "960px" })
+        .from(".section3__spanwrap div span", { y: "50px", scale: 0.1, ease: "power3.out", stagger: { axis: "x", from: "edge", each: 0.1, amount: 0.5 } });
+};
+
+const showbftext = () => {
+    let bttexttl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".section4_middle_blackfridaybanner",
+            start: "top 40%",
+            end: "bottom bottom",
+            scrub: true,
+        },
+    });
+    bttexttl
+        .from(".sec4_bf_spanwrap1 div span", { y: "55px", scale: 0, ease: "power3.out", stagger: { axis: "x", from: "center", each: 0.1, amount: 0.5 } })
+        .from(".sec4_bf_spanwrap2 div span", { y: "55px", scale: 0, ease: "power3.out", stagger: { axis: "x", from: "center", each: 0.1, amount: 0.5 } });
+};
+
+const showSec6 = () => {
+    let sec6tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".greet_container",
+            start: "top top",
+            end: "bottom top-=1000px",
+            pin: true,
+            pinSpacing: true,
+            scrub: true,
+        },
+    });
+    sec6tl.from(".spanwrap1 > span", { x: -320, opacity: 0 }).from(".spanwrap3", { x: -300, opacity: 0 });
+};
+
+const showSec6all = () => {
+    gsap.from("#outroimg1", {
+        scrollTrigger: {
+            trigger: ".greet_container",
+            start: "top 80%",
+            end: "bottom bottom",
+            scrub: true,
+        },
+        y: -960,
+        opacity: 0,
+    });
+    gsap.from("#outroimg2", {
+        scrollTrigger: {
+            trigger: ".greet_container",
+            start: "top 50%",
+            end: "bottom bottom",
+            scrub: true,
+        },
+        x: 400,
+        opacity: 0,
+        ease: "power2.out",
+    });
+    gsap.from("#outroimg3", {
+        scrollTrigger: {
+            trigger: ".greet_container",
+            start: "top 20%",
+            end: "bottom center",
+            scrub: true,
+        },
+        x: -400,
+        opacity: 0,
+        ease: "power3.out",
+    });
+    gsap.from(".spanwrap2", {
+        scrollTrigger: {
+            trigger: ".greet_container",
+            start: "top top-=600px",
+            end: "+=1000",
+            scrub: true,
+        },
+        y: -100,
+        opacity: 0,
+        ease: "power3.out",
+    });
+};
+
+/* 스크롤을 내리면 헤더가 뷰포트 상단에 고정되게 하는 함수 */
+const modifyHeaderFix = () => {
+    window.addEventListener('scroll', () => {
+        if(window.scrollY >=99){
+            document.querySelector('.header2').classList.add('active');
+        } else {
+            document.querySelector('.header2').classList.remove('active');
+        }
+    })
+}
+
+
+/* 카테고리를 클릭하면 메뉴가 나오는 함수 */
+let currentMenu = 0;
+let lotateArrow = 0;
+const slideCategory = () => {
+    $('.category-wrap').click(function(){
+        lotateArrow += 180;
+        if(currentMenu === 0) {
+            $('.category-section').stop().slideDown();
+            currentMenu = 1;
+        } else {
+            $('.category-section').stop().slideUp();
+            currentMenu = 0;
+        }
+        // $('.menuarrow').stop().toggleClass('active');
+        $('.menuarrow').css("transform", `rotate(${lotateArrow}deg)`);
+    })
+}
+
+
+const clickremoteBtn = () => {
+    $('.dotbtn').click(function(){
+        $('.dotbtn').toggleClass('active')
+        $('.controller').toggleClass('active')
+        remoteicons.forEach(ele => {
+            ele.classList.toggle('active');
+        })
+    })
+}
+/* 리모컨 */
+const scrollToRemote = () => {
+    for(let i = 0; i<toSections.length; i++){
+        toSections[i].addEventListener('click', () => {
+            switch(i){
+                case i=0 : {
+                    lenis.stop();
+                    window.scrollTo({
+                        top : document.querySelector('html').offsetTop
+                    })
+                    console.log('i = 0');
+                    lenis.start()
+                    break;
+                }
+                case i=1 : {
+                    lenis.stop();
+                    window.scrollTo({
+                        top : document.querySelector('.section4').offsetTop
+                    })
+                    console.log('i = 1');
+                    lenis.start();
+                    break;
+                }
+                case i=2 : {
+                    lenis.stop();
+                    window.scrollTo({
+                        top : document.querySelector('.hotitem_container').getBoundingClientRect().top + window.scrollY
+                    })
+                    console.log('i = 2');
+                    lenis.start();
+                    break;
+                }
+                case i=3 : {
+                    lenis.stop();
+                    alert("아직 구현중인 페이지입니다.")
+                    console.log('i = 3');
+                    lenis.start();
+                    break;
+                }
+            }
+        })
+    }
+}
+
+
+/* 프리뷰 */
+const previewImage = () => {
+    const previewsimg = document.querySelectorAll('.preview');
+    for(let i = 0; i < previewsimg.length; i++){
+        previewsimg[i].addEventListener('mouseenter', () => {
+            document.querySelector('#categorynavimg').src = categoryImgsrc[i];
+        })
+    }
+}
+/* 스크롤 부드럽기 조절 */
+const lenis = new Lenis({
+    duration: 2,
+});
+lenis.on("scroll", ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
+
+/* 배너섹션  */
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    disableOnInteraction:false,
+    speed: 1000,
+    autoplay: {
+        delay: 5000,
+    },
+    loop: true,
+    watchSlidesProgress: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+});
+/* 캐시미어 섹션 */
+var swiper = new Swiper(".cashmereSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    disableOnInteraction:false,
+    autoplay: {
+        delay:2000,
+    },
+    loop: true,
+    speed:1000,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    on : {
+        init : function(){
+            const thiscontainer = document.querySelector('.cashmereSwiper');
+            thiscontainer.addEventListener('mouseenter', () => {
+                this.autoplay.stop();
+            })
+            thiscontainer.addEventListener('mouseleave', () => {
+                this.autoplay.start();
+            })
+        }
+    }
+});
+
+
+
+modifyImgFilter();
+animateSection3();
+showbftext();
+showSec6all();
+showSec6();
+slideCategory();
+modifyHeaderFix();
+clickremoteBtn();
+scrollToRemote();
+previewImage();
+ScrollTrigger.create({
+    trigger: ".section5", // 고정할 섹션
+    start: "bottom bottom-=100",
+    end: "+=200%",
+    pin: true, // section1을 고정
+    pinSpacing: false, // 고정된 동안 다른 섹션의 공간 유지
+});
